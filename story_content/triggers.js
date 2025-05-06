ffa16d58aba32c5c1247c56c74035c2a010ab4f1
twhittaker06 <t.whittaker@mmu.ac.uk>
2025-05-06 16:26:32 +0100

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
